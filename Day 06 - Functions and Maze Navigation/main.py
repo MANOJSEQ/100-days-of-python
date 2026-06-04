670d3d01e2df80c7d50680fe20621227dbe5003b
Manoj Alexius Sequeira <manojsequeira17@gmail.com>
2026-06-04 22:52:56 +0100

    Create main.py

---
+++ Day 06 - Functions and Maze Navigation/main.py
@@ -0,0 +1,19 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
